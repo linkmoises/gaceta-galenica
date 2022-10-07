2842dbe3bf83ec36da9d3eca939e210561988be8
Moisés Serrano <linkmoises@gmail.com>
2022-10-06 21:44:05 -0500

    Versión preliminar de sitio web

---
+++ www/theme/akademia/footer.php
@@ -0,0 +1,31 @@
+<?php if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die('Por favor no cargue esta p&aacute;gina directamente. &iexcl;Gracias!'); }  ?>
+
+    <!-- Footer
+    ============================================= -->
+    <footer id="footer" class="dark border-0">
+
+      <div id="copyrights">
+        <div class="container center clearfix">
+          &#127279; Algunos derechos reservados CC-BY-NC-SA
+        </div>
+      </div>
+
+    </footer><!-- #footer end -->
+
+  </div><!-- #wrapper end -->
+
+  <!-- Go To Top
+  ============================================= -->
+  <div id="gotoTop" class="icon-angle-up"></div>
+
+  <!-- JavaScripts
+  ============================================= -->
+  <script src="js/jquery.js"></script>
+  <script src="js/plugins.min.js"></script>
+
+  <!-- Footer Scripts
+  ============================================= -->
+  <script src="js/functions.js"></script>
+
+</body>
+</html>
---
+++ www/theme/akademia/header.php
@@ -0,0 +1,95 @@
+<?php if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die('Por favor no cargue esta p&aacute;gina directamente. &iexcl;Gracias!'); }  ?>
+<!doctype html>
+<html dir="ltr" lang="es">
+<head>
+
+  <meta charset="utf-8">
+	<meta name="author" content="Moisés Serrano Samudio" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700|Roboto:300,400,500,700|Playfair+Display&display=swap" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link rel="stylesheet" href="css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="css/onepage.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+	<link rel="stylesheet" href="css/custom.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- Document Title
+	============================================= -->
+	<title>Gaceta Galénica</title>
+
+  <!-- Favicons
+  ============================================= -->
+  <link rel="apple-touch-icon" sizes="57x57" href="static/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="static/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="static/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="static/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="static/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="static/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="static/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="static/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="static/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="static/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="static/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png">
+  <link rel="manifest" href="static/manifest.json">
+
+</head>
+
+<body class="stretched">
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header header-size-custom" data-sticky-shrink="false">
+			<div id="header-wrap">
+				<div class="container">
+					<div class="header-row">
+
+						<!-- Logo
+						============================================= -->
+						<div id="logo">
+							<a href="index.html" class="standard-logo" data-dark-logo="images/titulo-w.png"><img src="images/logo.png" alt="Canvas Logo"></a>
+							<!--<a href="index.html" class="retina-logo" data-dark-logo="one-page/images/canvasone-dark@2x.png"><img src="one-page/images/canvasone@2x.png" alt="Canvas Logo"></a>-->
+						</div><!-- #logo end -->
+
+						<div id="primary-menu-trigger">
+							<svg class="svg-trigger" viewBox="0 0 100 100"><path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path><path d="m 30,50 h 40"></path><path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path></svg>
+						</div>
+
+						<!-- Primary Navigation
+						============================================= -->
+						<nav class="primary-menu">
+
+							<ul class="one-page-menu menu-container" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
+								<li class="menu-item">
+									<a href="#" class="menu-link" data-href="#wrapper"><div>Inicio</div></a>
+								</li>
+                <li class="menu-item">
+									<a href="#" class="menu-link" data-href="#section-archivo"><div>Archivo</div></a>
+								</li>
+								<li class="menu-item">
+									<a href="#" class="menu-link" data-href="#section-blog"><div>Editor</div></a>
+								</li>
+								<li class="menu-item">
+									<a href="#" class="menu-link" data-href="#section-contact"><div>Contacto</div></a>
+								</li>
+							</ul>
+
+						</nav><!-- #primary-menu end -->
+
+					</div>
+				</div>
+			</div>
+			<div class="header-wrap-clone"></div>
+		</header><!-- #header end -->
